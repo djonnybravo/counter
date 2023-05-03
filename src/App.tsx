6f968ecc4899bd0cfec4fb08376b0552d76e2a10
djonnybravo <djonnybravo29@gmail.com>
2023-05-03 12:21:25 +0300

    add counter components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
+import Counter from "./components/Counter/Counter";
+import CounterSettings from "./components/Counter/CounterSettings/CounterSettings";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [startValue, setStartValue] = useState(0)
+    const [counterValue, setCounter] = useState(startValue)
+    const [maxValue, setMaxValue] = useState(0)
+    const [message, setMessage] = useState('')
+    const [disableIncrementBtn, setDisableIncrementBtn] = useState(true)
+    // --------CALLBACKS-----------
+    const incrementCallback = () => {
+       // counterValue === maxValue ? setDisableIncrementBtn(true) : setCounter(counterValue + 1)
+        setCounter(counterValue + 1)
+    }
+    const resetCallback = () => {
+        setCounter(startValue)
+    }
+    const setSettings = () => {
+        localStorage.setItem('startValue', JSON.stringify(startValue))
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
+        setCounter(startValue)
+        setMessage('')
+        setDisableIncrementBtn(false)
+    }
+
+    // _________LOCALSTORAGE___________
+
+    let savedStartValue = localStorage.getItem('startValue')
+    let savedMaxValue = localStorage.getItem('maxValue')
+    const getValueFromLocalStorage = () => {
+
+        if (savedStartValue) {
+            let savedStartValueAsNumber = JSON.parse(savedStartValue)
+            setStartValue(savedStartValueAsNumber)
+        }
+        if (savedMaxValue) {
+            let savedMaxValueAsNumber = JSON.parse(savedMaxValue)
+            setMaxValue(savedMaxValueAsNumber)
+        }
+        }
+    useEffect(getValueFromLocalStorage, [])
+
+
+
+    return (
+        <div className="App">
+            <CounterSettings  setSettings={setSettings}
+                              startValue = {startValue}
+                              maxValue = {maxValue}
+                              setStartValue = {setStartValue}
+                              setMaxValue = {setMaxValue}
+                              setMessage = {setMessage}
+            />
+
+            <Counter counterValue={counterValue}
+                     incrementCallback={incrementCallback}
+                     resetCallback={resetCallback}
+                     startValue = {startValue}
+                     maxValue = {maxValue}
+                     message = {message}
+                     disableIncrementBtn = {disableIncrementBtn}
+            />
+        </div>
+    );
 }
 
 export default App;
